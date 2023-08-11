85e636e192a30bf25ec78521fa28d3305ef4ef44
wlaanait <wlaanait@gmail.com>
2023-08-10 18:47:11 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fonction
+ *
+ * Return: 0 when seccessful
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
