ebb6026300caae10fb4676e7a9ef777b4d708e64
wlaanait <wlaanait@gmail.com>
2023-08-10 17:45:20 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fonction
+ *
+ * return:always 0
+ */
+int main(void)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0)
+}
