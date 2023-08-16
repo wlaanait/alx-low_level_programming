6bac59a9d1bbfd63297c7ff878e51fad9a246c4f
wlaanait <wlaanait@gmail.com>
2023-08-15 18:04:44 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - function that print putchar
+ * main - function that print _putchar
  * Return: return 0;
- */ 
+ */
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
