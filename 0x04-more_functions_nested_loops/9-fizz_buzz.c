23c238a70ae1ed7a918c472c72fb05e7088910b2
wlaanait <wlaanait@gmail.com>
2023-08-18 18:14:15 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i = 1 ; i <= 100 ; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s", "FizzBuzz");
+		else if (i % 3 == 0)
 			printf("%s", "Fizz");
 		else if (i % 5 == 0)
 			printf("%s", "Buzz");
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz");
 		else 
 			printf("%d", i);
 		if (i != 100)
